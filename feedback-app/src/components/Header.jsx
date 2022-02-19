0fc987d3760f20999d9f39d81a1be13691c7696c
robbridges <robbridges@users.noreply.github.com>
2022-02-19 12:57:22 -0800

    More work on the state and working with props

--- feedback-app/src/components/Header.jsx
+++ feedback-app/src/components/Header.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 const Header = (props) => {
+  const headerStyles = {
+    text: 'FeedBack UI',
+    backgroundColor: props.bgColor,
+    color: props.textColor,
+  }
   return (
-    <header>
+    <header style={headerStyles}>
       <div className='container'>
         <h2>{props.text}</h2>
       </div>
     </header>
   )
+}
+
+Header.defaultProps = {
+  text:'Feedback UI',
+  bgColor: 'rgba(0,0,0,0.4)',
+  textColor: '#ff6a95'
 }
 
 export default Header;
